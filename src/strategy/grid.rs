a72ef9cb71f018392715ac3c32999f00fff4c984
subhransu <smohantty@gmail.com>
2025-12-13 16:28:23 +0900

    fix: Swap Total column for Size column in dashboard

--- src/strategy/grid.rs
+++ src/strategy/grid.rs
@@ -509,9 +509,9 @@ impl Strategy for GridStrategy {
                     <div class="col lvl">{}</div>
                     <div class="col price">{:.*}</div>
                     <div class="col dist">{:.2}%</div>
-                    <div class="col total">{:.*}</div>
+                    <div class="col size">{:.*}</div>
                 </div>"#,
-                width_pct, ask.level_idx, p_dec, ask.price, dist_pct, s_dec, ask.total
+                width_pct, ask.level_idx, p_dec, ask.price, dist_pct, s_dec, ask.size
             );
             rows_html.push_str(&row);
         }
@@ -541,9 +541,9 @@ impl Strategy for GridStrategy {
                     <div class="col lvl">{}</div>
                     <div class="col price">{:.*}</div>
                     <div class="col dist">{:.2}%</div>
-                    <div class="col total">{:.*}</div>
+                    <div class="col size">{:.*}</div>
                 </div>"#,
-                width_pct, bid.level_idx, p_dec, bid.price, dist_pct, s_dec, bid.total
+                width_pct, bid.level_idx, p_dec, bid.price, dist_pct, s_dec, bid.size
             );
             rows_html.push_str(&row);
         }
@@ -675,7 +675,7 @@ impl Strategy for GridStrategy {
                 <div class="col lvl">Lvl</div>
                 <div class="col price">Price</div>
                 <div class="col dist">Dist%</div>
-                <div class="col">Total</div>
+                <div class="col">Size</div>
             </div>
             <div class="clob-content" id="clobContent">
                 {rows_html}
